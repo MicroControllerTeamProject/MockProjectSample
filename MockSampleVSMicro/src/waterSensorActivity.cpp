12bbbf167adf244361e7d28b36318d409e90c5e2
Luigi Santagada <luigi.santagada@live.com>
2020-07-27 01:31:02 +0200

    ....

--- MockSampleVSMicro/src/waterSensorActivity.cpp
+++ MockSampleVSMicro/src/waterSensorActivity.cpp
@@ -6,7 +6,6 @@ waterSensorActivity::waterSensorActivity(){
 void waterSensorActivity::start(waterSensor ws, microInterface& microInterface, programStates& programStates)
 {
 	programStates._isWaterDetected  = ws.isThereWater(microInterface);
-	programStates._isWaterDetected;
 	if (programStates._isWaterDetected){
 		microInterface.printData("water is detected",true);
 	}
