dd1e469ca9659f38cf6be08b8fb56d462f63b0a4
Luigi Santagada <luigi.santagada@live.com>
2022-12-06 00:53:49 +0100

    .

---
+++ GarageMonitorSystem_Test/UnitTestArduinoProject.cpp
@@ -0,0 +1,90 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\GarageMonitorSystem\model\AnalogPort.h"
+#include "..\GarageMonitorSystem\model\DigitalPort.h"
+#include "..\GarageMonitorSystem\activity\SmokeActivity.h"
+#include "..\GarageMonitorSystem\activity\PirActivity.h"
+#include "..\GarageMonitorSystem\activity\DeviceActivity.h"
+#include "..\GarageMonitorSystem\business\GarageBusinessLayer.h"
+
+#include "src\extend.h"
+
+//https://docs.microsoft.com/it-it/visualstudio/test/writing-unit-tests-for-c-cpp?view=vs-2019
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestGarageMonitorSystem
+{
+	TEST_CLASS(UnitTestGarageMonitorSystem)
+	{
+	public:
+		TEST_METHOD(TestMethod_openTheGarageDoor)
+		{
+#pragma region repository mocked
+			Mock<MainRepository> mockedRepository;
+			MainRepository& mainRepository = mockedRepository.get();
+#pragma endregion Repository mocked
+
+#pragma region objects for test 
+			AnalogPort** analogSmokePorts = new AnalogPort * [1];
+			analogSmokePorts[0] = new AnalogPort("Smoke01", 14);
+			analogSmokePorts[0]->maxAlarmValueIn = 150;
+			analogSmokePorts[0]->minAlarmValueIn = 1;
+
+			DigitalPort** digitalPirPorts = new DigitalPort * [1];
+			digitalPirPorts[0] = new DigitalPort("Pir01", 4);
+			digitalPirPorts[0]->alarmTriggerOn = DigitalPort::AlarmOn::high;
+			digitalPirPorts[0]->direction = DigitalPort::PortDirection::input;
+#pragma endregion objects for test 
+
+			SmokeActivity* smokeActivity = new SmokeActivity(analogSmokePorts, 5, 1);
+			PirActivity* pirActivity = new PirActivity(digitalPirPorts, 1);
+
+#pragma region mocked methods 
+			When(Method(mockedRepository, analogReadm)).AlwaysReturn(100);
+			When(Method(mockedRepository, digitalReadm)).AlwaysReturn(1);
+#pragma endregion mocked methods 
+
+#pragma region Asserts 
+			GarageBusinessLayer* b = new GarageBusinessLayer();
+			Assert::AreEqual(true, b->canOpenTheDoor(mainRepository, smokeActivity, pirActivity));
+#pragma endregion Asserts
+			/*Assert::AreEqual("[|   ]o", garageDoorActivity->getBatteryGrafBarLevel(mainRepository, 0));*/
+		}
+		TEST_METHOD(TestMethod_cantOpenTheGarageDoor)
+		{
+#pragma region repository mocked
+			Mock<MainRepository> mockedRepository;
+			MainRepository& mainRepository = mockedRepository.get();
+#pragma endregion Repository mocked
+
+#pragma region objects for test 
+			AnalogPort** analogSmokePorts = new AnalogPort * [1];
+			analogSmokePorts[0] = new AnalogPort("Smoke01", 14);
+			analogSmokePorts[0]->maxAlarmValueIn = 150;
+			analogSmokePorts[0]->minAlarmValueIn = 1;
+
+			DigitalPort** digitalPirPorts = new DigitalPort * [1];
+			digitalPirPorts[0] = new DigitalPort("Pir01", 4);
+			digitalPirPorts[0]->alarmTriggerOn = DigitalPort::AlarmOn::high;
+			digitalPirPorts[0]->direction = DigitalPort::PortDirection::input;
+#pragma endregion objects for test 
+
+			SmokeActivity* smokeActivity = new SmokeActivity(analogSmokePorts, 5, 1);
+			PirActivity* pirActivity = new PirActivity(digitalPirPorts, 1);
+
+#pragma region mocked methods 
+			When(Method(mockedRepository, analogReadm)).AlwaysReturn(500);
+			When(Method(mockedRepository, digitalReadm)).AlwaysReturn(1);
+#pragma endregion mocked methods 
+
+#pragma region Asserts 
+			GarageBusinessLayer* b = new GarageBusinessLayer();
+			Assert::AreEqual(false, b->canOpenTheDoor(mainRepository, smokeActivity, pirActivity));
+#pragma endregion Asserts
+			/*Assert::AreEqual("[|   ]o", garageDoorActivity->getBatteryGrafBarLevel(mainRepository, 0));*/
+		}
+
+
+	};
+}
